ee5006572551cc957a5f9fea23238c60f30a07ba
algoritmamaratonu <canfurkan903@gmail.com>
2024-04-10 04:55:18 +0300

    first commit

---
+++ saler_mobil/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>a</Text>
+      <View>
+<TextInput  placeholder='reis'></TextInput>
+      </View>
+      
+     
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
